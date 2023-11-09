4c0e2e0690f5d259faa623983f9b2c998a639ac3
sakthivel <37198254+sakthivelj@users.noreply.github.com>
2023-11-09 04:46:22 +0000

    update vechile name

--- core/frontend/src/App.vue
+++ core/frontend/src/App.vue
@@ -275,11 +275,11 @@
     <new-version-notificator />
     <Wizard />
     <alerter />
-    <v-tour
+    <!-- <v-tour
       name="welcomeTour"
       :steps="steps.filter((step) => step?.filter_wifi_connected !== wifi_connected)"
       :callbacks="tourCallbacks"
-    />
+    /> -->
   </v-app>
   <div v-else>
     <services-scanner />
