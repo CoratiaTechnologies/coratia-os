d3e4647886a3257d9f1eb2651dd1b7238e72b8c7
sakthivel <37198254+sakthivelj@users.noreply.github.com>
2023-11-13 15:24:21 +0530

    Update color blue_robotics.js

--- core/frontend/src/assets/colors/blue_robotics.js
+++ core/frontend/src/assets/colors/blue_robotics.js
@@ -1,8 +1,8 @@
 /* eslint-disable */
 module.exports = {
-  BR_BLUE: '#2699D0',
-  MARINER_BLUE: '#135DA3',
-  BLUE_WHALE: '#012F46',
+  BR_BLUE: '#FFD700',
+  MARINER_BLUE: '#DAA520',
+  BLUE_WHALE: '#FFAC33',
   TETHER_YELLOW: '#FFE01B',
   TUNA: '#8C8C8C',
   OYSTER: '#403B3B',
